78c6010cd91e291b7969a0e5ff981f64a08ef580
kbladow83716 <kevin@bladow.us>
2024-06-30 08:36:58 -0600

    updated files

--- mysql-java/src/main/resources/projects_schema.sql
+++ mysql-java/src/main/resources/projects_schema.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS project;
 CREATE TABLE project(
 	project_id INT AUTO_INCREMENT NOT NULL,
 	project_name VARCHAR(128) NOT NULL,
-	estimate_hours DECIMAL(7,2),
+	estimated_hours DECIMAL(7,2),
 	actual_hours DECIMAL(7,2),
 	difficulty INT,
 	notes TEXT,
